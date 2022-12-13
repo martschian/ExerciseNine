30d2193b3a803ff993c80a6d5a496b684ed1d2a0
Håkan Hellgren <67669+martschian@users.noreply.github.com>
2022-12-13 14:13:46 +0100

    Clean-up

--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -1,31 +1,31 @@
 const list = document.querySelector('ul');
 
-const toggleDone = function (e) {
-  e.classList.toggle('done');
+const toggleDone = function (el) {
+  el.classList.toggle('done');
 };
 
-const removeItemFromList = function (e) {
-  console.log(e.target.parentElement);
-  list.removeChild(e.target.parentElement);
+const removeItemFromList = function (el) {
+  list.removeChild(el.parentElement);
 };
 
-const addItemToList = function (e) {
-  console.log(e.target[0].value);
-  const newItem = document.createElement('li');
-  const text = document.createTextNode(e.target[0].value);
+const addItemToList = function (el) {
+  const item = document.createElement('li');
+  const text = document.createTextNode(el[0].value);
   const button = document.createElement('button');
-  button.innerText = 'x';
-  button.addEventListener('click', (e) => removeItemFromList(e));
-  newItem.appendChild(text);
-  newItem.appendChild(button);
 
-  newItem.classList.add('list-group-item');
-  newItem.addEventListener('click', (e) => toggleDone(e.target));
-  list.appendChild(newItem);
-  e.target.reset();
+  button.setAttribute('type', 'button');
+  button.innerText = 'x';
+  button.addEventListener('click', (e) => removeItemFromList(e.target));
+
+  item.appendChild(text);
+  item.appendChild(button);
+  item.classList.add('list-group-item');
+  item.addEventListener('click', (e) => toggleDone(e.target));
+  list.appendChild(item);
+  el.reset();
 };
 
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  addItemToList(e);
+  addItemToList(e.target);
 });
